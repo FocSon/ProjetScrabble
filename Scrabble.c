582c0b91478d9af189710f1ef8cdeb52bf8c0d5c
FocSon <ifocson@gmail.com>
2019-10-19 12:38:47 +0200

    Images files + makefile + dico + .c file moificaton (affichage plateau)

--- Scrabble.c
+++ Scrabble.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#include "../lib/libgraphique.h"
+#include "lib/libgraphique.h"
+
+#define RESH 690
+#define RESV 690
 
 int main()
 	{
-	printf("Hello world");
+	ouvrir_fenetre(RESH, RESV);
+	Point init={0,0};
+	
+	afficher_image("Images/plateau.bmp", init);
+	actualiser();
+	
+	attendre_clic();
+	fermer_fenetre();
 	
 	return 0;
 	}
